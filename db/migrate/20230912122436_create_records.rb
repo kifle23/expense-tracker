58bf457ccc2cafd6c7a5671a8b327f604d4b7df8
kifle23 <kiflekifle23@gmail.com>
2023-09-12 17:07:51 +0300

    Create migration for records

---
+++ db/migrate/20230912122436_create_records.rb
@@ -0,0 +1,11 @@
+class CreateRecords < ActiveRecord::Migration[7.0]
+  def change
+    create_table :records do |t|
+      t.string :name
+      t.decimal :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
