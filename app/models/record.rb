248f0c6d7dd3b49bcd899d792ff05bd4913f6401
kifle23 <kiflekifle23@gmail.com>
2023-09-13 12:58:59 +0300

    Modify record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,8 @@
 class Record < ApplicationRecord
   belongs_to :user
+  has_many :category_records, dependent: :destroy
+  has_many :category, through: :category_records
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
