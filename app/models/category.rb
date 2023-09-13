34f05b9f32da0b7428f5638c2bd07bf6a885b101
kifle23 <kiflekifle23@gmail.com>
2023-09-13 12:58:41 +0300

    Modify category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :category_records, dependent: :destroy
+  has_many :records, through: :category_records, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
