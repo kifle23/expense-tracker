8a219f2633c9217750887ec35e6930e546d4e592
kifle23 <kiflekifle23@gmail.com>
2023-09-13 14:20:34 +0300

    Add spec for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  it 'should be invalid without an email' do
+    user = User.create(name: 'Ruby Guy', email: nil, password: 'pass_123', password_confirmation: 'pass_123')
+    expect(user).to_not be_valid
+  end
+  it 'should be valid' do
+    user = User.create(name: 'Ruby Guy', email: 'ruby_guy@ruby.com', password: 'pass_123', password_confirmation: 'pass_123')
+    expect(user).to be_valid
+  end
+end
