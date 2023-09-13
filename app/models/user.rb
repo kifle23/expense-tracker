5f92006204c8fb26b361a338f8f4c78f5f7b766b
kifle23 <kiflekifle23@gmail.com>
2023-09-13 12:58:08 +0300

    Modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+  
+    validates :name, presence: true, length: { minimum: 1, maximum: 150 }
+    has_many :categories, dependent: :destroy
+    has_many :records, dependent: :destroy
 end
+  
