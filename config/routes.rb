6f2201bcfffbb2a57ed3a3979b1805fb8deafae3
kifle23 <kiflekifle23@gmail.com>
2023-09-13 07:37:33 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  resources :categories, only: [:index, :new, :create, :destroy] do
+    resources :records, only: [:index, :new, :create]
+  end
+  root "categories#index"
 end
