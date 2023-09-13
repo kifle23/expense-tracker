eba3d72cba7446ac95e8d4e85adb9a9967958f78
kifle23 <kiflekifle23@gmail.com>
2023-09-13 14:37:18 +0300

    Add spec for category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Category, type: :model do
+  before :each do
+    @user = User.create(name: 'Ruby Guy', email: 'ruby_guy@ruby.com', password: 'pass_123', password_confirmation: 'pass_123')
+  end
+  it 'should be invalid without the icon' do
+    category = Category.create(name: 'Ruby Guy', icon: nil, user_id: @user.id)
+    expect(category).to_not be_valid
+  end
+  it 'should be valid' do
+    category = Category.create(name: 'Ruby Guy', icon: 'icon', user_id: @user.id)
+    expect(category).to be_valid
+  end
+end
